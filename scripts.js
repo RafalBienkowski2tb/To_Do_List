47b80096e1a9d2c5609a0f2a26e267458a53c24f
RafalBienkowski2tb <154457659+RafalBienkowski2tb@users.noreply.github.com>
2025-10-06 20:36:08 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,287 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+========================================================================================= ANSII Korwin ============================================================================================
+
+                                                                                                                                                                                                        
+                                                  @@@ @ @@@-    @+*  +@                ++@@ @@@@@@+ @     %+    @@@*      @    @  @   :@@ @@@@                                                          
+                                                     @@@        - #            *                      *                                 @    =                                                          
+                                                    -@@                                                                   %@            @@  @@                                                          
+                                                 @    @  -                @@                     @@                              +        @  @                                                          
+                                                 @ @  @          =%-    =      #   @%   + -                     @@  +@@                *  @@  @                                                         
+                                                   @ @@                                       @@@ *@      #@@+              @@         @@   @ #                                                         
+                                                 @ @ @  @@                                                   -  @@@ @@+      :.          @     @                                                        
+ .                                               @  -@ .     :  @        - -@@%@    @:     @%#@@@@@@@  -@@@@@@@@   @       @@@   =@            @@                                                       
+                                                    @@     -     @   *@@       + .@    @@@@*.                          @              @@@       @                                                       
+                                                @   *  @          :@.     @@@@@:                         @:     @                @      @@@   @ @                                                       
+                                                @  @  @                *                                   @.        =@@.        @                                                                      
+                                                   %                -                            @@                #             @         *     @*                                                     
+                                                                    +   :@%         .@          @       @              @         @@          :    @                                                     
+                                                      *- -   @#              =@          =+ +%+#   @#@     %@@@@@@@@ @  @@@@@@@@    -  @     @@@% @                                                     
+                                                   %  @              *@@@            :@ @@:@@         @@      @@@@@@@@@@@@@@@@@@@@@  @         @                                                        
+                                                   @- *            @           @@@@@@ =                  :=  @        :@    .  @@ @@ #         @@ @@                                                    
+                                                   @  @          @   @@   @@@@@@  @@@@@       @+ @@@@    @      :@@@@@      @      @@                                                                   
+                                                   @  @         @  @@ @@@@@  @   @     +@+=@@            @                  @@@@@   @        @   @@@@@:                                                 
+                                                   @  @       @   @@  @@@    *     @@@  @@@  *@   @@@  @@#       @@@  @@@@                      @@@@@@@@                                                
+                                                   *=            @@       @                 #   @@@  @@  @@@@@@@@@@@@@@@@@@@@@@       @       @ @@    @@@                                               
+                                              *@@@@@   @ @-    @@@    @@     @@@@@@@@@@@@@@@  :=@   % @        @               @@@@   @         @  @   @@@                                              
+                                            @@@@@@@    @       @ @@@@@    @@@@@@@@@@@@@@       @@-@@@   @@ @@@@@  @@@@@@@@@@@@@    @  @         @ @@@   @@                                              
+                                           @@@@@@@@    @          :     @@@@@           @@   @@@@  @@@@  @@:   @@@@@@@@@@@@@  @@@@@@        +  @    @@  @@                                              
+                                          @@@@@@@@@@@@        @      @@@@    @@@@@@@@@@@@ @  .@       @   @         @              @@@@@@     @@@    @@  @@                                             
+                                          @@@       @@         % @@@@   @@@@@@@@@@@    @  @@  @    @  @      @          @@@    @@             @@ @@@@ @  @@                                             
+                                         @@@@    @   @@        @     @@@@@@@     -.           @@+  @         %@@@@         @@@@  @@@    +      @   *@ :  @@                                             
+                                         @@ @   @@@@  @        @@@@@@:               @##   @@-@ @  -      @@     @@@ @@         @              @@   @ @  @@                                             
+                                         @@ @   @  @  @@@        @@@       +     @@       @  @  @                  @@@@@@@@@@@@@@   @@          @@  @ @ @@@                                             
+                                         @@@    @    *@@@@       @@ @@@    : @@   .@@@@@        @        :@@@           @@#@@@     *     +@     @@@@@ @ @@                                              
+                                          @@@   @  @  @ @@  @  @      @@@         @@     @@@@ @= @@      @    @@@+                 *              @@ @ #@@                                              
+                                           @@   @  @  @@@      *        =@@@@@@@@      @@@  @            @@@@@  @@@     #@  +           :        @   @@@=@                                              
+                                           @@@@    @  @@@                             @@  @=@ *    =         @@@ @@@@@@                              @@ @@                                              
+                                             @@  @ @    @@   @-  = +@   =@   @        @@ @    @@@@@@  @@       @@  @@          -@ @-    =         =@    @                                               
+                                             @@@     @@  @@     @: .                 @@ @@      @     @   @@@@@@ @  @@   @@%@@    @    @        @@     @@                                               
+                                              @@@ @@ @@@@ @                @@        @   @@@@@          @@@@@@    @  @@                        @      @@                                                
+                                               @@@  @   @ @                          @     @@@@@@@@@@@@@@@     @*  @  @@     *=@@    @ -         @@  @@                                                 
+                                               @@@@@@ @    @             @:       #@@@  @@     @@@@@@@@   @@@@@@    @  @             @       .@@@@@@@@                                                  
+                                                @@@    @ @ @       @@              :%  @@@@ @@@   @@@@@@@@@ @ @@ @@@ @  @     *@@@*@         @@ @@@@                                                    
+                                                 .@@     @@               @           @*      @ @@@ @@ @@@  @    @  @@@  @         #         @@                                                         
+                                                   @@@      :       :  *    @   @     @ @ @@@@@ @      @    @ @@      @@ @    * -@@          @@                                                         
+                                                    @@@@    @@                    @@@@  @     @ @  :        @  @    @@ @@@      :            @@                                                         
+                                                     @@@@@@@@@@                @ @@  @   @@ @      @@@@   @@@ @  @@@    @   @.@  +  @@  =   @@                                                          
+                                                       #@@@@ @@              + @   @ @  @   @ @@@@@   @@@@   .@@@@@@@  @    @ @* :          @@                                                          
+                                                              @@          @ @ @@  @@      @@@  @@@@@@@@@    @  @.   @@@@@ @ @  @ @          @@                                                          
+                                                              @@          @ @    @@@@ @@@@                   @  @@@@    @@  @@ %+     @    @@                                                           
+                                                               @@   @@    @ @ @  @   @@   @@@  @@@@@@@@@@@@@@ @@@@  @@@# @@@ @  @ @       @@@                                                           
+                                                               @@@  @ @     @ @: @@@ @  @@@@@@@@@#          @@@        @@@ @ @  @ .       @@                                                            
+                                                                @@          @ @    @@ @@+                      @@@@    @  @ @@ @   %    @@@                                                             
+                                                                 @@        @ @@  :@@  @@    @     * @@ @   @            @@  @  @        @@                                                              
+                                                                 -@@     % @%     @ @@                 @@    @@@@@@@        @      @   @@                                                               
+                                                                  @@@     -@   @@ @       @@@     @@@@  @@@@@@ @@@    *@@@@     @    - @@                                                               
+                                                                   @@@@      =@   @   @@@.   @@-@@ =@@      @        @    @@@   @   @  @@                                                               
+                                                                     @@.            @ @       @@@@    @ @              @     @ @       @@:                                                              
+                                                                      @@ @@    -  .@                          @@     @    @@@    @@@   @@@@@                                                            
+                                                                     @@@  @@        @  @    @@@ %@@         @@     @ = @         @#    @@@@@@@                                                          
+                                                                    @@@@    @           @       @@  @     =   @ @@@@ :        @@@    @@@@ @@@@@@                                                        
+                                                                  @@@@@@@   @@@+        @@@#                @ @           .          @@@@@  @@@@@@                                                      
+                                                                @@@@@@ @@@   @@@@          @@   @@@=            :    @ -     @       @@ @@@@ @@@@@@@@                                                   
+                                                              @@@@@@@ @@@@      @@@                 -     @@@                       @@   @@@@  @@@@@@@@@@                                               
+                                                           @@@@@@@    @@@@@        @:                  @    @  @          :         @@    @@@@@  @@@@@@@@@@@@                                           
+                                                      @@@@@@@@@@   %  @@@ @@    @  +@   =@@@@@     @@@@@            @@@@@@         @@     @@@@@@  @@@  @@@@@@@@@@@@@                                    
+                                                @@@@@@@@@@@@@         @@@  @@@               @@@@@@@@:   @@@  @@@@@                @@      @@@@@@@  @@     @@@@@@@@@@@@@@@@@@@                          
+                                            @@@@@@@@@@@@@         -   @@@   @@@=                  @                      # @      @@     %  @@@@@@@  @@   @@@@    @@@@@@@@@@@@@@@@@@@@@@                
+                                       @@@@@@@@@@@@       @           @@@    @@@@       @@@  -    @*          @@ @@ @      +     @@             @@@@ @@@       @@@          @@@@@@@@@@@@@@@@@@@         
+                                  @@@@@@@@@@@@      @@   @             @@      @@@@      +@*      @@ @ @         @# @@       +  @@               @@@@  @@ @       :@@@@@@@@*   @@@@@@@@@@@@@@@@@@@@@@@@ 
+                            @@@@@@@@@@@@@     @@@@@@@       =          @@@@      @@@@@        @  @@@ @ @    @@@@@@@# #     %@-@@@+            @@@@@@@@ @@@ @  @@      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+                                                                                                                                                                                                        
+
+*/
+
+class Task {
+  static nextId = 1;
+  constructor(text, id = null) {
+    this.id = id ?? Task.nextId++;
+    this.text = text;
+  }
+}
+
+const TaskApp = (() => {
+  const STORAGE_KEY = 'my_todo_tasks_v1';
+  let tasks = [];
+
+  const inputEl = document.getElementById('fname');
+  const addBtn = document.getElementById('addTaskBtn');
+  const taskContainer = document.querySelector('.task');
+
+  let listEl = document.getElementById('taskList');
+  if (!listEl) {
+    listEl = document.createElement('div');
+    listEl.id = 'taskList';
+    
+    listEl.style.padding = '0.6rem';
+    
+    listEl.style.maxHeight = '50vh';
+    listEl.style.overflowY = 'auto';
+    taskContainer.appendChild(listEl);
+  }
+
+  function save() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  }
+
+  function load() {
+    const raw = localStorage.getItem(STORAGE_KEY);
+    if (!raw) return;
+    try {
+      const parsed = JSON.parse(raw);
+      if (Array.isArray(parsed)) {
+        tasks = parsed.map(obj => {
+          const t = new Task(obj.text, obj.id);
+          if (typeof obj.id === 'number' && obj.id >= Task.nextId) {
+            Task.nextId = obj.id + 1;
+          }
+          return t;
+        });
+      }
+    } catch (e) {
+      console.warn('Nie udało się wczytać zadań z localStorage', e);
+      tasks = [];
+    }
+  }
+
+  function renderTask(t) {
+    const li = document.createElement('div');
+    li.dataset.id = t.id;
+    li.style.display = 'flex';
+    li.style.alignItems = 'center';
+    li.style.justifyContent = 'space-between';
+    li.style.padding = '0.6rem 0.4rem';
+    li.style.borderBottom = '1px solid rgba(255,255,255,0.08)';
+
+    const left = document.createElement('div');
+    left.style.display = 'flex';
+    left.style.alignItems = 'center';
+    
+
+    const cb = document.createElement('input');
+    cb.type = 'checkbox';
+    cb.title = 'Oznacz jako zrobione';
+    
+    
+    
+    cb.addEventListener('change', () => {
+      if (cb.checked) {
+        textSpan.style.textDecoration = 'line-through';
+        textSpan.style.opacity = '0.6';
+      } else {
+        textSpan.style.textDecoration = 'none';
+        textSpan.style.opacity = '1';
+      }
+    });
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = t.text;
+    textSpan.style.color = 'white';
+    textSpan.style.fontFamily = 'Verdana, Geneva, Tahoma, sans-serif';
+    
+    
+
+    left.appendChild(cb);
+    left.appendChild(textSpan);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.title = 'Usuń to zadanie';
+    removeBtn.style.border = 'none';
+    removeBtn.style.background = 'transparent';
+    removeBtn.style.cursor = 'pointer';
+
+    const trashIcon = document.createElement('img');
+    trashIcon.src = 'foto/kosz.png';
+    trashIcon.alt = 'Usuń';
+    trashIcon.style.width = '20px';
+    trashIcon.style.height = '20px';
+
+    removeBtn.appendChild(trashIcon);
+
+    removeBtn.addEventListener('click', () => {
+      removeTaskById(t.id);
+    });
+
+    li.appendChild(left);
+    li.appendChild(removeBtn);
+
+    return li;
+  }
+
+  function renderAll() {
+    listEl.innerHTML = '';
+    if (tasks.length === 0) {
+      const empty = document.createElement('li');
+      empty.textContent = 'Brak zadań. Wpisz tekst i kliknij "Dodaj" lub naciśnij Enter.';
+      empty.style.color = 'white';
+      empty.style.opacity = '0.9';
+      empty.style.fontFamily = 'Verdana, Geneva, Tahoma, sans-serif';
+      listEl.appendChild(empty);
+      return;
+    }
+
+    for (const t of tasks) {
+      const li = renderTask(t);
+      listEl.appendChild(li);
+    }
+  }
+
+  function addTask(text) {
+    const clean = (text || '').trim();
+    if (!clean) return;
+    const t = new Task(clean);
+    tasks.push(t);
+    save();
+    renderAll();
+  }
+
+  function removeTaskById(id) {
+    tasks = tasks.filter(t => t.id !== id);
+    save();
+    renderAll();
+  }
+
+  function init() {
+    load();
+    renderAll();
+
+    if (inputEl) {
+      inputEl.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          addTask(inputEl.value);
+          inputEl.value = '';
+        }
+      });
+    }
+
+    if (addBtn) {
+      addBtn.addEventListener('click', () => {
+        addTask(inputEl.value);
+        inputEl.value = '';
+      });
+    }
+  }
+
+  return {
+    init,
+    addTask,
+    removeTaskById
+  };
+})();
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', TaskApp.init);
+} else {
+  TaskApp.init();
+}
+
+
